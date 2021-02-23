92280df95b94d93630bc1ae77af6320114617feb
Andrea Gallo <andreagallo264@gmail.com>
2021-02-23 18:29:50 -0300

    react router applied

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+//Components
+import Header from './components/Header';
+import ProfileList from './components/ProfileList';
+
+
 function App() {
   return (
-    <div>
-      <h1>Profile app</h1>
-    </div>
+    <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/profiles">
+              <ProfileList/>
+          </Route>
+          <Route path="/">
+              <h1>404</h1>
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/ProfileList/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProfileList = () => {
+    return ( 
+        <h1>Profile list</h1>
+    );
+}
+ 
+export default ProfileList;
